89e18a8278bd656955f3e2324fc57a88e60b54ad
dishanksanghavi <dishanks1997@gmail.com>
2025-03-03 23:40:14 -0500

    03/03/25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+  <app-header />
+  <app-home />`,
+  styles: ``
 })
 export class AppComponent {
-  title = 'helloWorld';
+  title = 'Hello World Test';
 }
